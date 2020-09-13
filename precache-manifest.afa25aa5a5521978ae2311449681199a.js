5b7d7d7a8d6abc794c80bf6ac5ebf07478918b43
HuyAtreides <phangiahuy2001@gmail.com>
2020-09-13 10:08:22 +0700

    Updates

---
+++ precache-manifest.afa25aa5a5521978ae2311449681199a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ed63ee14e68cc4e67f3ac836acd32a3",
+    "url": "/JavaScriptCaculator/index.html"
+  },
+  {
+    "revision": "36c78bf5ebfee5efc317",
+    "url": "/JavaScriptCaculator/static/css/main.c2b67e49.chunk.css"
+  },
+  {
+    "revision": "83e428bd28e6827bc20c",
+    "url": "/JavaScriptCaculator/static/js/2.2ad255e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/JavaScriptCaculator/static/js/2.2ad255e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36c78bf5ebfee5efc317",
+    "url": "/JavaScriptCaculator/static/js/main.2df1e48a.chunk.js"
+  },
+  {
+    "revision": "724e82866e18b68ad72d",
+    "url": "/JavaScriptCaculator/static/js/runtime-main.403501bf.js"
+  }
+]);

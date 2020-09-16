4b207a8d9024429df425f281bedf36dc95e7664b
HuyAtreides <phangiahuy2001@gmail.com>
2020-09-16 19:01:55 +0700

    updated

--- src/HandleButton/handleCaculate.js
+++ src/HandleButton/handleCaculate.js
@@ -1,21 +1,24 @@
 export default function handleCaculate(prevState) {
-	let operations = prevState.operation.replace("x", "*");
-	if (!/=/.test(prevState.operation))
-		if (/([0-9.]+[-+*/]{1,2}[0-9.]+|^[+-]*[0-9.]+$)/.test(operations)) {
-			// eslint-disable-next-line
-			let result = Function(`return ${operations}`)();
-			return {
-				operation:
-					prevState.operation +
-					"=" +
-					Math.round(result * 100000000000000) / 100000000000000,
-				display: Math.round(result * 100000000000000) / 100000000000000,
-			};
-		} else if (/^[0-9]+$[-*+/]*/.test(operations)) {
-			operations = operations.replace(/[-+*/]+/, "");
-			return {
-				operation: operations + "=" + operations,
-				display: operations,
-			};
-		}
+  let operations = prevState.operation.replace("x", "*");
+  if (!/=/.test(prevState.operation))
+    if (
+      /([0-9.]+[-+*/]{1,2}[0-9.]+|^[+-]*[0-9.]+$)/.test(operations) &&
+      /[-+0-9]/.test(operations[0])
+    ) {
+      // eslint-disable-next-line
+      let result = Function(`return ${operations}`)();
+      return {
+        operation:
+          prevState.operation +
+          "=" +
+          Math.round(result * 100000000000000) / 100000000000000,
+        display: Math.round(result * 100000000000000) / 100000000000000,
+      };
+    } else if (/^[0-9]+$[-*+/]*/.test(operations)) {
+      operations = operations.replace(/[-+*/]+/, "");
+      return {
+        operation: operations + "=" + operations,
+        display: operations,
+      };
+    }
 }
